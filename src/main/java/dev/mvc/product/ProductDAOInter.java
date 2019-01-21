86efed924d7dc38ee63c768bb19581ab00c224a6
seung <seung@DESKTOP-SS09BI9>
2019-01-21 12:25:53 +0900

    0121 seungq

--- src/main/java/dev/mvc/product/ProductDAOInter.java
+++ src/main/java/dev/mvc/product/ProductDAOInter.java
@@ -1,6 +1,17 @@
 package dev.mvc.product;
 
+import java.util.List;
+
+import dev.mvc.category.CategoryVO;
+import dev.mvc.country.CountryVO;
+
 public interface ProductDAOInter {
 
-  public ProductVO product_name_search(int review_no);
+  public abstract int create(ProductVO productVO);
+
+  public abstract List<CategoryVO> select_category_one();
+  
+  public abstract List<CategoryVO> select_category_two(int categoryno);
+
+  public abstract List<CountryVO> country_list();
 }
